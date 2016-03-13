3392f2b987de449bf7d2e3559508254cab1a4f85
Brijesh Bittu <brijeshb42@gmail.com>
2016-03-14 00:28:21 +0530

    Include package data

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='querybuilder',
-    packages=['querybuilder'],
     version='0.1.0',
     include_package_data=True,
+    packages=find_packages(),
     description=(
         'A library to build human '
         'readable SQL query string using a pythonic API'),
