f54b5562d63c7251b282970f6b252e8b39aca8fc
Brijesh Bittu <brijeshb42@gmail.com>
2016-03-14 00:09:49 +0530

    Fixed URLs

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
+
 setup(
     name='querybuilder',
     packages=['querybuilder'],
@@ -10,8 +11,8 @@ setup(
     # long_description=open("README.md").read(),
     author='Brijesh Bittu',
     author_email='brijeshb42@gmail.com',
-    url='https://github.com/brijeshb42/qbuilder',
-    download_url='https://github.com/brijeshb42/qbuilder/tarball/0.1.0',
+    url='https://github.com/brijeshb42/querybuilder',
+    download_url='https://github.com/brijeshb42/querybuilder/tarball/0.1.0',
     keywords=['SQL', 'peewee', 'querybuilder'],
     license="http://www.opensource.org/licenses/mit-license.php",
     classifiers=[],
