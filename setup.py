94f64621fe1658395d64eba7d05411daeb84c633
Brijesh Bittu <brijeshb42@gmail.com>
2016-03-15 17:14:15 +0530

    add long desc in setup

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+long_desc = ""
+with open("README.md") as f:
+    long_desc = f.read()
+
 setup(
     name='querybuilder',
     version='0.1.1',
@@ -8,7 +12,7 @@ setup(
     description=(
         'A library to build human '
         'readable SQL query string using a pythonic API'),
-    # long_description=open("README.md").read(),
+    long_description=long_desc,
     author='Brijesh Bittu',
     author_email='brijeshb42@gmail.com',
     url='https://github.com/brijeshb42/querybuilder',
